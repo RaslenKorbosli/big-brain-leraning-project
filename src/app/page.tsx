69d6c01f54e50195c68642385acf5f41064b5043
Raslen <raslen.korbosli01@gmail.com>
2024-07-23 17:12:26 +0100

    sitting up clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+'use client';
 import { Button } from '@/components/ui/button';
+import {
+  SignInButton,
+  SignOutButton,
+  useAuth,
+  UserButton,
+  useUser,
+} from '@clerk/nextjs';
+import { Authenticated, Unauthenticated } from 'convex/react';
 
 export default function Home() {
+  const auth = useUser();
+  console.log(auth);
   return (
     <div>
       <Button>Click meee</Button>
+      <Unauthenticated>
+        <SignInButton mode="modal" />
+      </Unauthenticated>
+      <Authenticated>
+        <UserButton />
+        {/* <Content /> */}
+      </Authenticated>
     </div>
   );
 }
